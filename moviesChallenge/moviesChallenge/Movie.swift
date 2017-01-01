3651ee092d7de617a1f5ac032a664a63373c07e6
jacqschweiger <jacqschweiger@gmail.com>
2017-01-01 15:59:06 -0500

    created movie class

--- moviesChallenge/moviesChallenge/Movie.swift
+++ moviesChallenge/moviesChallenge/Movie.swift
@@ -32,54 +32,28 @@ class Movie {
     
     init(title: String, year: String, rated: String, released: String, runtime: String, genre: String, director: String, writer: String, actors: String, plot: String, language: String, country: String, awards: String, poster: String, metaScore: String, imdbRating: String, imdbVotes: String, imdbID: String, type: String, response: String) {
         
-        self.title: String
-        self.year: String
-        self.rated: String
-        self.released: String
-        self.runtime: String
-        self.genre: String
-        self.director: String
-        self.writer: String
-        self.actors: String
-        self.plot: String
-        self.language: String
-        self.country: String
-        self.awards: String
-        self.poster: String
-        self.metaScore: String
-        self.imdbRating: String
-        self.imdbVotes: String
-        self.imdbID: String
-        self.type: String
-        self.response: String
-        
-        
-        
-        
+        self.title = title
+        self.year = year
+        self.rated = rated
+        self.released = released
+        self.runtime = runtime
+        self.genre = genre
+        self.director = director
+        self.writer = writer
+        self.actors = actors
+        self.plot = plot
+        self.language = language
+        self.country = country
+        self.awards = awards
+        self.poster = poster
+        self.metaScore = metaScore
+        self.imdbRating = imdbRating
+        self.imdbVotes = imdbVotes
+        self.imdbID = imdbID
+        self.type = type
+        self.response = response
     }
-    
-    
-    
-    "Title": "Titanic",
-    "Year": "1997",
-    "Rated": "PG-13",
-    "Released": "19 Dec 1997",
-    "Runtime": "194 min",
-    "Genre": "Drama, Romance",
-    "Director": "James Cameron",
-    "Writer": "James Cameron",
-    "Actors": "Leonardo DiCaprio, Kate Winslet, Billy Zane, Kathy Bates",
-    "Plot": "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
-    "Language": "English",
-    "Country": "USA",
-    "Awards": "Won 11 Oscars. Another 110 wins & 73 nominations.",
-    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BZDNiMjE0NDgtZWRhNC00YTlhLTk2ZjItZTQzNTU2NjAzNWNkXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
-    "Metascore": "74",
-    "imdbRating": "7.7",
-    "imdbVotes": "811,881",
-    "imdbID": "tt0120338",
-    "Type": "movie",
-    "Response": "True"
+
     
     
 }
